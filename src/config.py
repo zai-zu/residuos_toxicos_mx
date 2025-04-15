3c92ab7fc85a078a4a85d8765f0420250a79affc
Zaira Chavarin <zai.azuchm@gmail.com>
2025-04-15 10:02:57 -0600

    :wrench: fix/update gitignore

--- src/config.py
+++ src/config.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 df_residuos_tox_grouped_generacion_anio = pd.read_csv("C:/Users/zaire/Portafolio/Data/residuos_tox_mex/data/processed/df_residuos_tox_grouped_generacion_anio.csv")
 
+
